788074e2f5ef27e4000147d85661166b7d2fe7d0
Sean Fagan <faganseanp@gmail.com>
2023-09-13 11:08:18 -0600

    tutorial might be AI generated, not coherent. Switching to new tutorial

--- main.py
+++ main.py
@@ -5,13 +5,27 @@ import pygame
 # Updated to conform to flake8 and black standards
 from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT
 
-pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+
+# Define a Player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+
+pygame.init()
+
 # Create the screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Instatiate player. Right now, this is just a rectangle.
+player = Player()
 
 # Variable to keep the main loop running
 running = True
@@ -30,20 +44,20 @@ while running:
         elif event.type == QUIT:
             running = False
 
-# Fill the screen with white
-screen.fill((255, 255, 255))
+    # Fill the screen with white
+    screen.fill((0, 0, 0))
 
-# Create a surface and pass in a tuple containing its length and width
-surf = pygame.Surface((50, 50))
+    # Create a surface and pass in a tuple containing its length and width
+    surf = pygame.Surface((50, 50))
 
-# Give the surface a color to separate it from the background
-surf.fill((0, 0, 0))
-rect = surf.get_rect()
+    # Give the surface a color to separate it from the background
+    surf.fill((0, 0, 0))
+    rect = surf.get_rect()
 
-# This line says 'Draw surf onto the screen at the center'
-# 2 args - the surface to draw, the location at which to draw it on the source surface
-screen.blit(surf, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
-pygame.display.flip()
+    # This line says 'Draw surf onto the screen at the center'
+    # 2 args - the surface to draw, the location at which to draw it on the source surface
+    screen.blit(surf, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+    pygame.display.flip()
 
 # Put the center of the surf at the center of the display
 surf_center = (
